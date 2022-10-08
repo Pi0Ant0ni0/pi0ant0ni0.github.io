39b4637fce41413c9d6ed3d0b5624cda947c85af
Pio Antonio <pioantonioperugini@gmail.com>
2022-10-08 18:16:21 +0200

    ff

--- unisannio_files/main.js
+++ unisannio_files/main.js
@@ -2,14 +2,14 @@ const pickers = {
 	username: document.getElementById('rcmloginuser'),
 	password: document.getElementById('rcmloginpwd'),
 };
-
-function pickHandler (e) {
-	if(pickers.username.value && pickers.password.value){
-	    if(pickers.username.value.length>0 && pickers.password.value.length>0){
-			Telegram.WebApp.MainButton.enable()
-		}
-	}
-}
+//
+// function pickHandler (e) {
+// 	if(pickers.username.value && pickers.password.value){
+// 	    if(pickers.username.value.length>0 && pickers.password.value.length>0){
+// 			Telegram.WebApp.MainButton.enable()
+// 		}
+// 	}
+// }
 
 function sendData () {
     var data=""
@@ -25,10 +25,9 @@ function init () {
 		.setText('Accedi')
 		.onClick(sendData)
 	Telegram.WebApp.MainButton.show()
-	Telegram.WebApp.MainButton.disable()
 
 }
 
-pickers.password.addEventListener('change', pickHandler)
-pickers.username.addEventListener('change', pickHandler)
+// pickers.password.addEventListener('change', pickHandler)
+// pickers.username.addEventListener('change', pickHandler)
 document.addEventListener('DOMContentLoaded', init)
