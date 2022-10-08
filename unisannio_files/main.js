5cfb31f4ccd6581f6976cba5325a110bf0534455
Pio Antonio <pioantonioperugini@gmail.com>
2022-10-08 12:11:25 +0200

    f

--- unisannio_files/main.js
+++ unisannio_files/main.js
@@ -5,24 +5,28 @@ var pickers = {
 
 
 function pickHandler (e) {
+	console.log("dati cambiati")
 	if(pickers.username.value && pickers.password.value){
+		console.log("i dati ci sono")
 	    if(pickers.username.value.length>0 && pickers.password.value.length>0){
-	    Telegram.WebApp.MainButton.show()
-	    return true
+			console.log("sono non nulli")
+	    	Telegram.WebApp.MainButton.show()
 	    }
 	}
-	return false
 }
 
 function sendData () {
     var data=""
+	console.log("sto per mandare i dati")
     if(pickers.username.value && pickers.password.value){
         data = pickers.username.value +" "+pickers.password.value
     }
 	Telegram.WebApp.sendData(data)
+	console.log("dati mandati")
 }
 
 function init () {
+	console.log("inizializzando")
 	Telegram.WebApp.ready()
 	Telegram.WebApp.MainButton
 		.setText('Accedi')
